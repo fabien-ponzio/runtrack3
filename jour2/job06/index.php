c1a45c1def3b0a3bc46f26833a4fb7ce1b7f8c7a
Fabien <fabien.ponzio@laplateforme.io>
2021-03-09 19:01:13 +0100

    INVOCATION

--- jour2/job06/index.php
+++ jour2/job06/index.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <script type="text/javascript" src="script.js"></script>
+    <link rel="stylesheet" href="job05.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    
+    <script type="text/javascript" src="script.js"></script>
 </body>
 </html>
---
+++ jour2/job06/script.js
@@ -0,0 +1,8 @@
+let save = ""; 
+document.addEventListener("keydown", function konamie(ninjutsu) {
+  save += ninjutsu.key; 
+  console.log(save); 
+  if (save == "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightba") {
+    document.write('KUCHIYOSE NO JUTSU : LA PULATEFORMU')
+  }
+})
